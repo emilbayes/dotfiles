796402b78c3ef2573ce40f27c0ab097ba2f2ba6a
Emil Bay <github@tixz.dk>
2015-12-15 16:13:47 +0100

    Add atom topic

---
+++ atom/configure.sh
@@ -0,0 +1,11 @@
+if [ -f $(which atom) ]
+then
+   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+   eval "$(sed -E 's/^/apm install &/' $DIR/apm)"
+
+   mkdir ~/.atom
+   cp $DIR/keymap.cson $DIR/snippets.cson $DIR/config.cson ~/.atom
+else
+   error "atom could not be installed\nsince Homebrew is not installed."
+fi
