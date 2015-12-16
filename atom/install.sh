d130005fbf45b59f2f24aa0c9718000b8886fdbc
Emil Bay <github@tixz.dk>
2015-12-16 14:27:15 +0100

    Remove stuff that's done at config time

--- atom/install.sh
+++ atom/install.sh
@@ -3,11 +3,6 @@ then
    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
    brew cask install atom
-
-   eval "$(sed -E 's/^/apm install &/' $DIR/apm)"
-
-   mkdir ~/.atom
-   cp $DIR/keymap.cson $DIR/snippets.cson $DIR/config.cson ~/.atom
 else
    error "atom could not be installed\nsince Homebrew is not installed."
 fi
